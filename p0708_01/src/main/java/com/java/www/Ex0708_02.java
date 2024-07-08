ae3e8c68fbecc3053d94aa3c0dd6a1069dfa144d
igyuwon <igyuwon60@gmail.com>
2024-07-08 18:19:23 +0900

    스프링 maven

---
+++ p0708_01/src/main/java/com/java/www/Ex0708_02.java
@@ -0,0 +1,18 @@
+package com.java.www;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Ex0708_02 {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/java/www/beans.xml");
+
+        Pencil pencil = ctx.getBean("Pencil",Pencil.class);
+        pencil.use();
+
+        ctx.close();
+
+
+
+    }
+}
---
+++ p0708_01/src/main/java/com/java/www/P070801Application.java
@@ -0,0 +1,13 @@
+package com.java.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P070801Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P070801Application.class, args);
+    }
+
+}
